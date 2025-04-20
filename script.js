a1840746cfeca47d8a82ac9cec37a3fcca536d7f
parikshaprasadh1 <ep24b014@smail.iitm.ac.in>
2025-04-20 11:05:12 +0530

    calendar working

--- script.js
+++ script.js
@@ -29,3 +29,44 @@ else if(e.target.tagName === "SPAN"){
 e.target.parentElement.remove();
 }
 }, false);
+const monthYear = document.getElementById("month-year");
+const daysContainer = document.getElementById("days");
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const today = date.getDate();
+
+const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+monthYear.innerText = `${months[month]} ${year}`;
+
+  // Weekday headers
+weekDays.forEach(day => {
+const div = document.createElement("div");
+div.classList.add("day");
+div.innerText = day;
+daysContainer.appendChild(div);
+});
+
+const firstDay = new Date(year, month, 1).getDay();
+const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  // Blank slots
+for (let i = 0; i < firstDay; i++) {
+const blank = document.createElement("div");
+blank.classList.add("date");
+daysContainer.appendChild(blank);
+}
+
+// Dates
+for (let i = 1; i <= daysInMonth; i++) {
+const dateDiv = document.createElement("div");
+dateDiv.classList.add("date");
+dateDiv.innerText = i;
+if (i === today) {
+dateDiv.classList.add("today"); // Highlight today
+}
+daysContainer.appendChild(dateDiv);
+}
